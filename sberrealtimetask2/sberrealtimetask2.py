83b1c5146d8af5c4a169c51890a918a040e81c6d
Ilya <ilya.goldovskiy@gmail.com>
2022-08-29 17:51:08 +0300

    Update sberrealtimetask2.py

--- sberrealtimetask2/sberrealtimetask2.py
+++ sberrealtimetask2/sberrealtimetask2.py
@@ -1,7 +1,7 @@
 import os
 import hyperloglog
 import time
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from hdfs import Config
 import subprocess
@@ -23,12 +23,8 @@ BATCH_TIMEOUT = 2  # Timeout between batch generation
 ssc = StreamingContext(sc, BATCH_TIMEOUT)
 dstream = ssc.queueStream(rdds=batches)
 
-seg_iphone = hyperloglog.HyperLogLog(0.01)
-seg_firefox = hyperloglog.HyperLogLog(0.01)
-seg_windows = hyperloglog.HyperLogLog(0.01)
-
 finished = False
-collected = False
+printed = False
 
 
 def set_ending_flag(rdd):
@@ -38,11 +34,10 @@ def set_ending_flag(rdd):
 
 
 def parse_to_hll(rdd):
-    global collected
     global seg_iphone
     global seg_firefox
     global seg_windows
-    rdd.count()
+    # rdd.count()
     for line in rdd.collect():
         user_id = line.split('\t')[0]
         user_agent = line.split('\t')[1]
@@ -52,11 +47,72 @@ def parse_to_hll(rdd):
             seg_iphone.add(user_id)
         if 'Firefox' in user_agent:
             seg_firefox.add(user_id)
-        collected = True
+
+
+def print_only_at_the_end(rdd):
+    global printed
+    rdd.count()
+    if finished and not printed:
+        for line in rdd.takeOrdered(1):
+            print(line)
+        printed = True
+
+
+def print_always(rdd):
+    l = rdd.collect()[0]
+    print('seg_windows: {}'.format( len( l['seg_windows'] ) ))
+    print('seg_firefox: {}'.format( len( l['seg_firefox'] ) ))
+    print('seg_iphone:  {}'.format( len( l['seg_iphone']  ) ))
+    print()
+
+    seg_windows_hll = hyperloglog.HyperLogLog(0.01)
+    seg_firefox_hll = hyperloglog.HyperLogLog(0.01)
+    seg_iphone_hll = hyperloglog.HyperLogLog(0.01)
+
+    for line in l['seg_windows']:
+        seg_windows_hll.add(line)
+
+    for line in l['seg_firefox']:
+        seg_firefox_hll.add(line)
+
+    for line in l['seg_iphone']:
+        seg_iphone_hll.add(line)
+
+    print('seg_windows_hll: {}'.format( len( seg_windows_hll ) ))
+    print('seg_firefox_hll: {}'.format( len( seg_firefox_hll ) ))
+    print('seg_iphone_hll:  {}'.format( len( seg_iphone_hll ) ))
+    print()
+
+
+def aggregator(values, old):
+    if old is None:
+        d = {'seg_windows': [],
+             'seg_iphone': [],
+             'seg_firefox': []
+             }
+        return d
+    for line in values:
+        user_info = line.split('\t')
+        user_id = user_info[0]
+        user_agent = user_info[1]
+        if 'Windows' in user_agent:
+            old['seg_windows'].append(user_id)
+        if 'iPhone' in user_agent:
+            old['seg_iphone'].append(user_id)
+        if 'Firefox' in user_agent:
+            old['seg_firefox'].append(user_id)
+    return old
 
 
 dstream.foreachRDD(set_ending_flag)
-dstream.foreachRDD(parse_to_hll)
+
+dstream \
+    .flatMap(lambda line: line.split('\n')) \
+    .map(lambda line: ('res', line)) \
+    .updateStateByKey(aggregator) \
+    .map(lambda x: x[1]) \
+    .foreachRDD(print_always)
+
 
 ssc.checkpoint('./checkpoint{}'
                .format(time.strftime("%Y_%m_%d_%H_%M_%s", time.gmtime())))  # checkpoint for storing current state
@@ -65,6 +121,7 @@ while not finished:
     time.sleep(0.01)
 ssc.stop()
 
-d = {'seg_windows': len(seg_windows), 'seg_iphone': len(seg_iphone), 'seg_firefox': len(seg_firefox)}
-for k in sorted(d, key=d.get, reverse=True):
-    print("{}\t{}".format(k, d[k]))
+
+# d = {'seg_windows': len(seg_windows), 'seg_iphone': len(seg_iphone), 'seg_firefox': len(seg_firefox)}
+# for k in sorted(d, key=d.get, reverse=True):
+#     print("{}\t{}".format(k, d[k]))
